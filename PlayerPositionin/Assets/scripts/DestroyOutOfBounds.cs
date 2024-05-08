6fc0a7c355139fdd1da78150e1986760a99bbf5d
katelyn <crespok@roverkids.org>
2024-05-08 13:06:21 -0400

    finished 2.2

--- PlayerPositionin/Assets/scripts/DestroyOutOfBounds.cs
+++ PlayerPositionin/Assets/scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
         }
-
 
     }
 }
